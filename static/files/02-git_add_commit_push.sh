789155e0a6979379dc9a25a4c5a1dd5475737533
ChudyWoody <zgredek94@gmail.com>
2025-03-05 00:19:26 +0100

    poprawiony base url w hugo.toml na chudywoody.github.io/zgre_blog_repo_test. Kiedy robię lokalnie hugo serve--baseURL="http://localhost:1313/" to zdjęcia działają, a przy zmienionym na repo samo hugo serve zdjęcia nie działają

--- static/files/02-git_add_commit_push.sh
+++ static/files/02-git_add_commit_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add -A
-git commit -m "poprawiony skrypt dla formatowania zdjęć i ich adresów, oraz poprawiony base url w hugo.toml"
+git commit -m "poprawiony base url w hugo.toml na chudywoody.github.io/zgre_blog_repo_test. Kiedy robię lokalnie hugo serve--baseURL=\"http://localhost:1313/\" to zdjęcia działają, a przy zmienionym na repo samo hugo serve zdjęcia nie działają"
 git push 
